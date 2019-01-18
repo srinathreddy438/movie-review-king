ac7ff0de1cb3be7101a306df93110e3bc673793c
Srinath Reddy Isukapalli <344584@narayanahealth.org>
2019-01-19 00:01:03 +0530

    fixed library issue

--- src/app/trending-videos/trending-videos.page.ts
+++ src/app/trending-videos/trending-videos.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Slides } from '@ionic/angular';
 
 @Component({
   selector: 'app-trending-videos',
@@ -6,15 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trending-videos.page.scss'],
 })
 export class TrendingVideosPage implements OnInit {
+  @ViewChild('mySlider', { read: Slides }) slides: Slides;
   videoUrl1: string;
   videoUrl2: string;
   videoUrl3: string;
+  slideOpts: any;
   constructor() { }
 
   ngOnInit() {
     this.videoUrl1 = 'https://www.youtube.com/embed/MTXXMDfIicA?rel=0';
     this.videoUrl2 = 'https://www.youtube.com/embed/otAcYFVFE7k';
     this.videoUrl3 = 'https://www.youtube.com/embed/HS8Df6ngZ24';
+    this.slides.options = {
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      }
+    };
   }
 
+  slidesDidLoad(slides: Slides) {
+    this.slides.startAutoplay();
+  }
 }
