fe8c049de1875ceab832cdeac7e9d7751f4562b0
Srinath Reddy Isukapalli <344584@narayanahealth.org>
2019-01-19 13:02:01 +0530

    fixed all slider issues

--- src/app/photos/photos.page.ts
+++ src/app/photos/photos.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { PhotosService } from 'src/app/services/photo-service';
 import { Router } from '@angular/router';
+import { AccountsService } from '../services/account-service';
 class PhotoModel {
   constructor(
     public imageName?: string,
@@ -19,9 +20,14 @@ class PhotoModel {
 export class PhotosPage implements OnInit {
   slideOpts: any;
   photosList: any;
-  constructor(public photosService: PhotosService, private router: Router) { }
+  isAdmin: boolean;
+  constructor(public accountsService: AccountsService, public photosService: PhotosService, private router: Router) { }
 
   ngOnInit() {
+    if (this.accountsService && this.accountsService.getLoginInfo() && this.accountsService.getLoginInfo().userName === 'srinath440') {
+      this.isAdmin = true;
+    }
+    this.photosList = [];
     this.getPhotos();
     this.slideOpts = {
       autoplay: true
@@ -29,8 +35,23 @@ export class PhotosPage implements OnInit {
   }
 
   getPhotos() {
-    this.photosService.getAllPhotosList().then((data: Response) => {
-      this.photosList = data;
+    this.photosService.getAllPhotosList().then((data: PhotoModel[]) => {
+      const initData = data;
+      const uniqNames = [];
+      initData.forEach((item: PhotoModel) => {
+        const checkExist = uniqNames.findIndex((name) => {
+          return name === item.groupByName;
+        });
+        if (checkExist === -1) {
+          uniqNames.push(item.groupByName);
+        }
+      });
+      uniqNames.forEach((name) => {
+        const result = initData.filter((item) => {
+          return  name === item.groupByName;
+        });
+        this.photosList.push(result);
+      });
     });
   }
 
